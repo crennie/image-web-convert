055e1823fd731af83fe28125e2a8f0b63a17df22
Chris Rennie <crennie.cr@gmail.com>
2025-08-18 17:44:53 -0700

    Initial commit of nx generated app skeleton

---
+++ apps/web/app/app.tsx
@@ -0,0 +1,12 @@
+import { Nav } from '@image-web-convert/ui';
+
+export function App() {
+    return (
+        <div>
+            <Nav />
+            <main className="min-h-100vh bg-lime-300">Sample Home Page</main>
+        </div>
+    );
+}
+
+export default App;
